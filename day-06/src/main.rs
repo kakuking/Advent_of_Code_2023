6ddba51c9290184fed16cb42e2763917c4d05e0f
Dubey, Kartikeya <kdubey@ea.com>
2024-03-18 14:41:29 +0530

    day 06

--- day-06/src/main.rs
+++ day-06/src/main.rs
@@ -1,3 +1,21 @@
+
 fn main() {
-    println!("Hello, world!");
+    let times: Vec<f64> = vec![60808676f64];
+    let distances: Vec<f64> = vec![601116315591300f64];
+
+    let mut ret : f64= 1.0;
+
+    for i in 0..1 {
+        let t_squared = times[i] * times[i];
+        let four_d = 4.0 * distances[i];
+
+        let diff = t_squared - four_d;
+        let mut lower = 0.5 * (times[i] - diff.sqrt()) + 1.0;
+        lower = lower.floor();
+        let upper = times[i] - lower;
+        let num = upper - lower + 1.0;
+
+        ret = ret * num;
+    }
+    print!("{ret}")
 }
